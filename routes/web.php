b804d4f7bd38e3b29e321a07abb7d00b69081a6b
Yoshimi-I <89241539+yoshimi-I@users.noreply.github.com>
2022-01-08 02:03:12 +0900

    fix: Target class [〇〇〇Controller] does not exist. のエラーの解消

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 //ブログ一覧画面を表示
-Route::get('/', 'BlogController@showList')->name
-('blogs');
+Route::get('/', 'App\Http\Controllers\BlogController@showList')->name('blogs');
