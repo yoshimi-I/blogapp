b94ef6447e23dcfa1a501ec6ffa63a4165119c3d
Yoshimi-I <89241539+yoshimi-I@users.noreply.github.com>
2022-01-08 01:00:18 +0900

    create: list.blade.php

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    //
+    /*
+    * ブログ一覧を表示する
+    * @return view
+    */
+    
+    public function showList()
+    {
+        return view('blog.list');
+    }
 }
