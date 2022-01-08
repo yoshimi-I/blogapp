ba7c0a5e37998b1703da148d12d3d1e051c2ee18
Yoshimi-I <89241539+yoshimi-I@users.noreply.github.com>
2022-01-08 17:34:07 +0900

    add: header,footerファイルに処理を分割

--- resources/views/blog/list.blade.php
+++ resources/views/blog/list.blade.php
@@ -1,5 +1,6 @@
 @extends('layout')
 @section('content')
+@section('title','ブログ一覧')
 <div class="container">
 <div class="row">
   <div class="col-md-8 col-md-offset-2">
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,33 +2,20 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>ブログ</title>
+    <title>@yield('title')</title>
     <link rel="stylesheet" href="/css/app.css">
     <script src="/js/app.js" defer></script>
 </head>
 <body>
     <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">ブログ</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">ブログ一覧 <span class="sr-only"></span></a>
-      <a class="nav-item nav-link" href="#">ブログ投稿</a>
-    </div>
-  </div>
-</nav>
+    @include('header')
     </header>
     <br>
     <div class="container">
         @yield('content')
     </div>
     <footer class="footer bg-dark  fixed-bottom">
-    <div class="container text-center">
-    <span class="text-light">誼のブログ</span>
-</div>
+    @include('footer')
     </footer>
 </body>
 </html>
